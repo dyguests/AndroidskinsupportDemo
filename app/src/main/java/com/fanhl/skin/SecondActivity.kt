d1a8112a78625536f1807d611a62572d2256face
fanhl <fanhonglin@klicen.com>
2019-04-03 10:07:48 +0800

    jump

--- app/src/main/java/com/fanhl/skin/SecondActivity.kt
+++ app/src/main/java/com/fanhl/skin/SecondActivity.kt
@@ -1,8 +1,10 @@
 package com.fanhl.skin
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -14,6 +16,17 @@ class SecondActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+    }
+
+    companion object {
+        /**
+         * 启动SecondActivity
+         *
+         * @param context 上下文
+         */
+        fun launch(context: Context) {
+            context.startActivity(Intent(context, SecondActivity::class.java))
         }
     }
 }
