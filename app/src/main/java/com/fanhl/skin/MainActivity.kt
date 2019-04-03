d28b1e82233936b729876ba56db3bc5efd75cb26
fanhl <fanhonglin@klicen.com>
2019-04-03 10:32:08 +0800

    res-red

--- app/src/main/java/com/fanhl/skin/MainActivity.kt
+++ app/src/main/java/com/fanhl/skin/MainActivity.kt
@@ -3,12 +3,15 @@ package com.fanhl.skin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import skin.support.SkinCompatManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_skin_default.setOnClickListener { SkinCompatManager.getInstance().restoreDefaultTheme() }
+        btn_skin_red.setOnClickListener { SkinCompatManager.getInstance().loadSkin("red", SkinCompatManager.SKIN_LOADER_STRATEGY_BUILD_IN) }
         btn_next.setOnClickListener { SecondActivity.launch(this@MainActivity) }
     }
 }
